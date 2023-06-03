8d4cba3a44ebd6927e14f754c32b5c189626e4f6
Juarez <juarezhenriquelisboa@gmail.com>
2023-06-02 21:37:15 -0300

    unidade 2

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/b9/80e32ac32200001e1de9ee52d09cba9d
@@ -0,0 +1,5 @@
+package br.com.tarefas.controller;
+
+public class TarefaController {
+
+}
---
+++ minhas-tarefas/src/main/java/br/com/tarefas/model/TarefaStatus.java
@@ -0,0 +1,6 @@
+package br.com.tarefas.model;
+
+public enum TarefaStatus {
+
+	ABERTO, EM_ANDAMENTO, CONCLUIDA, CANCELADA
+}
